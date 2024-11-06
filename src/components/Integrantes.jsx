1517076f67eb14dc95fdf796c4d55a6835b3ee36
Saul Gabriel <saulgabriel069@gmail.com>
2024-11-06 16:35:08 +0000

    Final

--- src/components/Integrantes.jsx
+++ src/components/Integrantes.jsx
@@ -5,10 +5,10 @@ import davi from "../assets/davi.svg"
 
 export function Integrantes() {
   const integrantes = [
-    { src: saul, nome: 'Saul Gabriel', desc: 'Olá seja bem vindo, ao TEASense!', cor:'rgb(3, 147, 100, 0.7)' },
-    { src: taina, nome: 'Tainá Dos Santos', desc: 'Olá seja bem vindo, ao TEASense!', cor:'rgb(0, 0, 0, 0.7)'},
-    { src: esther, nome: 'Esther Santos', desc: 'Olá seja bem vindo, ao TEASense!', cor:'rgb(122, 103, 53, 0.7)'},
-    { src: davi, nome: 'Davi Da Hora', desc: 'Olá seja bem vindo, ao TEASense!', cor:'rgb(112, 194, 199, 0.7)' },
+    { src: saul, nome: 'Saul Gabriel', desc: 'Olá seja bem vindo, ao TEASense! Sou Saul tenho 17 anos, e gosto muito de carros e futebol.', cor:'rgb(3, 147, 100, 0.7)' },
+    { src: taina, nome: 'Tainá Dos Santos', desc: 'Olá seja bem vindo, ao TEASense! Sou Tainá tenho 18 anos, e gosto muito de Rosa e Maquiagem.', cor:'rgb(0, 0, 0, 0.7)'},
+    { src: esther, nome: 'Esther Santos', desc: 'Olá seja bem vindo, ao TEASense! Sou Esther tenho 18 anos, e gosto muito de futebol, jogos online.', cor:'rgb(122, 103, 53, 0.7)'},
+    { src: davi, nome: 'Davi Da Hora', desc: 'Olá seja bem vindo, ao TEASense! Sou Davi tenho 18 anos, e gosto de artes e carros.', cor:'rgb(112, 194, 199, 0.7)' },
   ];
 
   return (
